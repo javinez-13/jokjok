826fe400e8e2c855370941766089d09f11a201bd
javinez-13 <javinezsarra@gmail.com>
2024-12-21 17:43:55 +0800

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,22 @@
+{
+    "name": "Random Joke Generator",
+    "short_name": "Joke Generator",
+    "description": "Get a random joke with the click of a button!",
+    "start_url": "/",
+    "display": "standalone",
+    "background_color": "#f7f7f7",
+    "theme_color": "#4CAF50",
+    "icons": [
+      {
+        "src": "12.jpg",
+        "sizes": "192x192",
+        "type": "image/jpg"
+      },
+      {
+        "src": "12.jpg",
+        "sizes": "512x512",
+        "type": "image/jpg"
+      }
+    ]
+  }
+  
